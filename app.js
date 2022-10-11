f67489449302437c8a637611d6ba3da836eb3f75
ilham suandi <ilham15suandi@gmail.com>
2022-10-11 22:36:41 +0700

    fix laggy and loading screen issue

--- app.js
+++ app.js
@@ -1,41 +1,29 @@
+// loading
+const loading = document.getElementById("loader-wrapper");
+const html = document.querySelector("html");
+const animation = document.getElementById("loading-animation");
+
+document.addEventListener("DOMContentLoaded", init);
+function init() {
+  onload = function () {
+    animation.addEventListener("ended", function () {
+      loading.classList.toggle("close");
+      setTimeout(() => {
+        html.classList.remove("loading");
+      }, 100);
+
+      setTimeout(() => {
+        loading.remove();
+      }, 200);
+    });
+  };
+}
+
+// mobile menu
 const menuToggle = document.querySelector(".hamburger-menu input");
 const navbar = document.querySelectorAll(".inner-nav ul");
 const navlist = document.querySelectorAll(".inner-nav li");
 const checkBox = document.querySelector(".checkbox");
-const carousel = document.querySelectorAll(".swiper-slide");
-var slidenum = 0;
-const loadingDuration = document.getElementById("loading").duration * 1000;
-
-// loading
-const loading = document.getElementById("loader-wrapper");
-const html = document.querySelector("html");
-document.addEventListener("DOMContentLoaded", init);
-function init() {
-  setTimeout(() => {
-    loading.classList.toggle("close");
-  }, 3000);
-  setTimeout(() => {
-    html.classList.remove("loading");
-  }, 3200);
-
-  setTimeout(() => {
-    loading.remove();
-  }, 3500);
-}
-
-// onload = function () {
-//   setTimeout(() => {
-//     loading.classList.toggle("close");
-//   }, 3000);
-//   setTimeout(() => {
-//     html.classList.remove("loading");
-//   }, 3200);
-//   setTimeout(() => {
-//     loading.remove();
-//   }, 3500);
-// };
-
-// mobile menu
 menuToggle.addEventListener("click", function () {
   for (j = 0; j < navbar.length; j++) {
     navbar[j].classList.toggle("open");
@@ -51,29 +39,8 @@ for (i = 0; i < navlist.length; i++) {
   });
 }
 
-// carousel algorithm
-var playSlider;
-var repeater = () => {
-  playSlider = setInterval(function () {
-    slidenum++;
-    if (slidenum > carousel.length - 1) {
-      slidenum = 0;
-    }
-
-    if (slidenum != 0) {
-      carousel[slidenum - 1].classList.remove("swiper-slide-active");
-    } else {
-      carousel[carousel.length - 1].classList.remove("swiper-slide-active");
-    }
-
-    carousel[slidenum].classList.add("swiper-slide-active");
-    console.log(slidenum);
-    // console.log(carousel.length);
-  }, 4000);
-};
-// repeater();
-
 // carousel current use
+const carousel = document.querySelectorAll(".swiper-slide");
 const swiper = new Swiper(".swiper", {
   speed: 1000,
   centeredSlides: true,
@@ -95,11 +62,6 @@ const swiper = new Swiper(".swiper", {
     disableOnInteraction: false,
   },
 
-  // If we need pagination
-  // pagination: {
-  //   el: '.swiper-pagination',
-  // },
-
   // Navigation arrows
   navigation: {
     nextEl: ".swiper-button-next",
@@ -120,17 +82,6 @@ const swiper2 = new Swiper(".swiper-2", {
   fadeEffect: {
     crossFade: true,
   },
-  // breakpoints: {
-  //   // when window width is >= 576px
-  //   100: {
-  //     slidesPerView: 1,
-  //     spaceBetween: 10,
-  //   },
-  //   835: {
-  //     slidesPerView: 1,
-  //     spaceBetween: 50,
-  //   },
-  // },
 
   autoplay: {
     delay: 5000,
